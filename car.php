e1c71c5801e8953574f772303d4584a46a2bd763
Kelli Sommerdyke & Phillip Shannon <student@epicodus.com>
2015-08-03 11:53:46 -0700

    completed constructors & private properties

--- car.php
+++ car.php
@@ -1,36 +1,46 @@
 <?php
     class Car
     {
-        public $make_model;
-        public $price;
-        public $miles;
+        private $make_model;
+        private $price;
+        private $miles;
+
+        function __construct($make, $price, $miles)
+        {
+            $this->make_model = $make;
+            $this->price = $price;
+            $this->miles = $miles;
+        }
+
+        function getMakeModel()
+        {
+            return $this->make_model;
+        }
+        function setMakeModel($model)
+        {
+            $this->make_model = $model;
+        }
+        function getPrice()
+        {
+            return $this->price;
+        }
+
+        function getMiles()
+        {
+            return $this->miles;
+        }
     }
 
-    $porsche = new Car();
-    $porsche->make_model = "2014 Porsche 911";
-    $porsche->price = 114991;
-    $porsche->miles = 7864;
-
-    $ford = new Car();
-    $ford->make_model = "2011 Ford F450";
-    $ford->price = 55995;
-    $ford->miles = 14241;
-
-    $lexus = new Car();
-    $lexus->make_model = "2013 Lexus RX 350";
-    $lexus->price = 44700;
-    $lexus->miles = 20000;
-
-    $mercedes = new Car();
-    $mercedes->make_model = "Mercedes Benz CLS550";
-    $mercedes->price = 39900;
-    $mercedes->miles = 37979;
+    $porsche = new Car("2014 Porsche 911", 114991, 7864);
+    $ford = new Car("2011 Ford F450", 55995, 14241);
+    $lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+    $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
 
     $cars = array($porsche, $ford, $lexus, $mercedes);
 
     $cars_matching_search = array();
     foreach ($cars as $car) {
-        if ($car->price < $_GET["price"]) {
+        if ($car->getPrice() < $_GET["price"]) {
             array_push($cars_matching_search, $car);
         }
     }
@@ -46,10 +56,13 @@
     <ul>
         <?php
             foreach ($cars_matching_search as $car) {
-                echo "<li> $car->make_model </li>";
+                $model = $car->getMakeModel();
+                $price = $car->getPrice();
+                $miles = $car->getMiles();
+                echo "<li> $model </li>";
                 echo "<ul>";
-                    echo "<li> $$car->price </li>";
-                    echo "<li>Miles: $car->miles </li>";
+                    echo "<li> $$price </li>";
+                    echo "<li>Miles: $miles </li>";
                 echo "</ul>";
 
             }
